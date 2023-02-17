9f524f5bb3bacb12f349af9a0863210b208c6589
jake <jongwon5113@gmail.com>
2023-02-17 17:46:31 +0900

    잘 꾸며보자

--- app/src/main/java/com/jake5113/tp7recyclerview/ItemAdapter.java
+++ app/src/main/java/com/jake5113/tp7recyclerview/ItemAdapter.java
@@ -1,8 +1,11 @@
 package com.jake5113.tp7recyclerview;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -22,24 +25,35 @@ public class ItemAdapter extends RecyclerView.Adapter<ItemAdapter.VH> {
     @NonNull
     @Override
     public VH onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View itemView = LayoutInflater.from(context).inflate(R.layout.item, parent, false);
+        VH holder = new VH(itemView);
+        return holder;
     }
 
     @Override
     public void onBindViewHolder(@NonNull VH holder, int position) {
-
+        Item item = items.get(position);
+        holder.tvQuotes.setText(item.quotes);
+        holder.tvName.setText(item.name);
+        holder.imgCar.setImageResource(item.profileImg);
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return items.size();
     }
 
     class VH extends RecyclerView.ViewHolder{
-
+        TextView tvName;
+        TextView tvQuotes;
+        ImageView imgCar;
 
         public VH(@NonNull View itemView) {
             super(itemView);
+
+            tvName = itemView.findViewById(R.id.tv_name);
+            tvQuotes = itemView.findViewById(R.id.tv_quotes);
+            imgCar = itemView.findViewById(R.id.iv_car);
         }
     }
 }
--- app/src/main/java/com/jake5113/tp7recyclerview/MainActivity.java
+++ app/src/main/java/com/jake5113/tp7recyclerview/MainActivity.java
@@ -1,16 +1,34 @@
 package com.jake5113.tp7recyclerview;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+
+    String[] names;
+    String[] quotes;
+    ArrayList<Item> items = new ArrayList<>();
+    RecyclerView recyclerView;
+    ItemAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        names = getResources().getStringArray(R.array.name);
+        quotes = getResources().getStringArray(R.array.quotes);
+
+        for (int i = 0; i < names.length; i++) {
+            items.add(new Item(quotes[i], names[i], R.drawable.car_01 + i));
+        }
+        recyclerView = findViewById(R.id.recyclerview);
+        adapter = new ItemAdapter(this, items);
+        recyclerView.setAdapter(adapter);
 
     }
 }
